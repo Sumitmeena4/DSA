f287dfef466eac0ac3d709cde648b7644fb42a18
Sumit Meena <180838892+Sumit-Meena4@users.noreply.github.com>
2025-03-20 10:55:43 +0530

    Add files via upload

--- Strings.java
+++ Strings.java
@@ -8,11 +8,23 @@ public class Strings {
        //System.out.println(substring(name, 3,7));
       
     }
-    // public static String substring(String str, int si, int ei){
-    //    String substr = "";
-    //    for(int i=si; i<ei; i++){
-    //     substr += str.charAt(i);
-    //    }
-    //    return substr;
-    // }
+    public static void printvowel(String str){
+        for(int i =0; i<str.length(); i++){
+            if(str.charAt(i)== 'a' || str.charAt(i) =='e' ||str.charAt(i) =='i'|| str.charAt(i) =='o'||str.charAt(i) =='u'){
+                System.out.println(str.charAt(i) + " Vowel");
+            } 
+        }
+    }
+    public static void printletters(String str){
+        for(int i =0; i<str.length(); i++){
+            System.out.print(str.charAt(i) + " ");
+        }
+    }
+    public static String substring(String str, int si, int ei){
+       String substr = "";
+       for(int i=si; i<ei; i++){
+        substr += str.charAt(i);
+       }
+       return substr;
+    }
 }
